9bf2936c323f73d4c4a16e993f586be62e622fb0
omer.khan <omer.khan@fdmgroup.com>
2022-03-28 01:37:37 -0400

    First Commit

---
+++ springboot-java-CRUD-web-app/src/test/java/com/java/springbootjavaCRUD/SpringbootJavaCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.springbootjavaCRUD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootJavaCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
